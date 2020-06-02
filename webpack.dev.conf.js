1dbbacb9c68ac423803e918797ae388e7e3d2427
AkperovaE <akperova.katerina@yandex.ru>
2020-06-02 16:53:05 +0300

    add const webpack, add sourceMapDevToolPlugin

--- webpack.dev.conf.js
+++ webpack.dev.conf.js
@@ -1,8 +1,13 @@
+const webpack = require ("webpack")
 const merge = require("webpack-merge")
 const baseWebpackConfig = require("./webpack.base.conf")
 const devWebpackConfig = merge(baseWebpackConfig, {
     mode: "development",
-    plugins: []
+    plugins: [
+        new webpack.SourceMapDevToolPlugin({
+            filename: "[file].map"
+        })
+    ]
 })
 
 module.exports = new Promise((resolve, reject) => {
