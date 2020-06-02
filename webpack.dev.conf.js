d4ae24de79653e3c2f67b5eeecfa5e650e2f2627
AkperovaE <akperova.katerina@yandex.ru>
2020-06-02 16:53:59 +0300

    cheap-module-eval-source-map

--- webpack.dev.conf.js
+++ webpack.dev.conf.js
@@ -3,6 +3,7 @@ const merge = require("webpack-merge")
 const baseWebpackConfig = require("./webpack.base.conf")
 const devWebpackConfig = merge(baseWebpackConfig, {
     mode: "development",
+    devtool: "cheap-module-eval-source-map",
     plugins: [
         new webpack.SourceMapDevToolPlugin({
             filename: "[file].map"
