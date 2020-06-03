66255b52a001d71ad4791c7acc693f44206510cb
AkperovaE <akperova.katerina@yandex.ru>
2020-06-03 17:30:00 +0300

    webpack plugins' paths

--- webpack.base.conf.js
+++ webpack.base.conf.js
@@ -1,6 +1,7 @@
 const path = require("path")
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
-
+const CopyWebpackPlugin = require("copy-webpack-plugin")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 const PATHS = {
     src: path.join(__dirname, "./src"),
     dist: path.join(__dirname, "./dist"),
@@ -63,6 +64,15 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: `${PATHS.assets}css/[name].css`
 
-        })
+        }),
+        new HtmlWebpackPlugin({
+            hash: false,
+            template: `${PATHS.src}/index.html`,
+            filename: "./index.html"
+        }),
+        new CopyWebpackPlugin([
+            { from: `${PATHS.src}/img`, to: `${PATHS.assets}img` },
+            { from: `${PATHS.src}/static`, to: " " }
+        ])
     ]
 }
